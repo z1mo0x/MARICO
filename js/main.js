96f7889c4ae31347991d6fec9a9da9e891c0e571
Z1mo0x <droweckiy@mail.ru>
2023-02-24 13:11:42 +0300

    all resolutions adaptive

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-const burger = document.querySelector(".header__burger");
-const list = document.querySelector(".header__list");
+const burger = document.querySelector(".header__burger"),
+  list = document.querySelector(".header__list");
 
 burger.addEventListener("click", () => {
   burger.classList.toggle("active-burger");
